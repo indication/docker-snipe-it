814348b7711c17c8b8e22d6fc3b33160cceaef2f
in_dow <in_dow@hotmail.com>
2019-05-17 02:03:26 +0900

    Reduce entry-point works

--- snipe/entrypoint_snipe.sh
+++ snipe/entrypoint_snipe.sh
@@ -32,10 +32,5 @@ done
 
 chown -R www-data:www-data /var/lib/snipeit/data/* /var/lib/snipeit/dumps /var/lib/snipeit/keys
 
-# If the Oauth DB files are not present copy the vendor files over to the db migrations
-if [ ! -f "/var/www/html/database/migrations/*create_oauth*" ]
-then
-  cp -a /var/www/html/vendor/laravel/passport/database/migrations/* /var/www/html/database/migrations/
-fi
 
 exec $@
